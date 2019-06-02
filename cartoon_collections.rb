0da6ef798461d3284339dec8ee59e53ac5898d9a
assane savadogo <assane.savadogo81@gmail.com>
2019-06-02 02:50:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(array)
-array.each.with_index(1) {|name, index| puts "#{name}. #{index}"}
+array.each.with_index(1) {|name, index| puts "#{index}. #{name}"}
 end
 
 def summon_captain_planet# code an argument here
