8ba9d3348bd58f66f62e97d1ed440f7ef6bc715d
assane savadogo <assane.savadogo81@gmail.com>
2019-06-02 02:56:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ array.each.with_index(1) {|name, index| puts "#{index}. #{name}"}
 end
 
 def summon_captain_planet(array)
-  
+  array.collect do {|name| name.capita
 end
 
 def long_planeteer_calls# code an argument here
